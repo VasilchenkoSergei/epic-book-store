cf2fe0bb9b369dc079322cbd2eee4f1d171cccef
VasilchenkoSergei <chainsaw86@mail.ru>
2018-12-02 01:36:33 +0300

    768 1280 80%

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,90 @@
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
+// // function ready(fn) {
+// //   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+// //     fn();
+// //   } else {
+// //     document.addEventListener('DOMContentLoaded', fn);
+// //   }
+// // }
+// //
+// // ready(function(){
+// //   console.log('DOM ready');
+// // });
+
+
+
+// $(document).ready(function(){
+
+// $('.promo__burger').on('click', function() {
+//   $('.main-nav').toggleClass('.active');
+//   $('.main-nav').toggle();
+// });
+
+// // !!!!!!!!!!!!!!SLIDER!!!!!!!!!!!!!!!!!!!!
+
+
+// $('.slider').owlCarousel ({
+//   dots: false,
+//   loop: true,
+//   center: true,
+//   nav: true,
+//   responsive : {
+//       0 : {
+//           items : 1,
+//           navText: ["<img src='img/icons/arrow_left.svg'>", "<img src='img/icons/arrow_right.svg'>"],
+//       }
 //   }
-// }
-//
-// ready(function(){
-//   console.log('DOM ready');
+
+// });
+
+// $('.catalog__tabs').owlCarousel ({
+//   dots: false,
+//   loop: true,
+//   center: false,
+//   nav: false
+
+// });
+// // $('.sizebox-list').owlCarousel ({
+// //   loop: true,
+// //   responsive : {
+// //       0 : {
+// //           items : 1,
+// //       },
+// //   }
+// // });
+
+// // $('.appearbox-list').owlCarousel ({
+// //   loop: true,
+// //   margin: 10,
+// //   responsive : {
+// //       0 : {
+// //           items : 1,
+// //       },
+// //       767 : {
+// //           items : 2,
+// //           dots: false,
+// //           nav: true,
+// //           navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
+// //       }
+// //   }
+// // });
+
+// // $('.reviews__list').owlCarousel ({
+// //   loop: true,
+// //   dots: true,
+// //   responsive : {
+// //       0 : {
+// //           items : 1,
+// //       },
+// //       950 : {
+// //           items : 1,
+// //           dots: false,
+// //           nav: true,
+// //           navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
+// //       }
+// //   }
+// // });
+
+
+
+
 // });
