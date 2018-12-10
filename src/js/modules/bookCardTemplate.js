eec3f7c77acd80282fb311b445890cf71c110586
VasilchenkoSergei <chainsaw86@mail.ru>
2018-12-10 08:02:40 +0300

    deleted error add and commit, recommendation page fixes

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -6,12 +6,12 @@ const bookCardTemplate = {
     return `<article class="product-card-mini">
               <a class="product-card-mini__picture-wrap" href="#">
                   <picture class="product-card-mini__picture">
-                  <img class="product-card-mini__img" src="img/${data.uri}.png" alt="${data.uri}" width="210" height="300"></picture>
+                  <img class="product-card-mini__img" src="img/books/${data.uri}.png" alt="${data.uri}" width="210" height="300"></picture>
               </a>
               <a class="product-card-mini__title-wrap" href="">
                   <h3 class="product-card-mini__title">${data.name}</h3>
               </a>
-              <p class="product-card-mini__text">${data.desc}</p>
+              <p class="product-card-mini__descr">${data.desc}</p>
               <span class="product-card-mini__price">${data.price} &#8381;</span>
             </article>`;
   }
