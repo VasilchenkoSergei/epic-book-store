e728da4885fa569e1ffc58ada6492b21dbe95290
VasilchenkoSergei <chainsaw86@mail.ru>
2018-12-23 23:25:18 +0300

    recommendation page grid all width, fix 320 and 768 width, parting pug book-block

--- src/js/modules/bookCardTemplate.js
+++ src/js/modules/bookCardTemplate.js
@@ -3,13 +3,13 @@ const bookCardTemplate = {
   tag: 'article',
   tagClass: '.product-card-mini',
   setContent: function(data) {
-    return `<a class="product-card-mini__picture-wrap" href="inner.html">
+    return `<a class="product-card-mini__picture-wrap" href="description.html">
               <picture class="product-card-mini__picture">
                 <source media="(min-width: 1920px)" srcset="img/books/${data.uri}_2x.png 2x"/>
                 <img class="product-card-mini__img" src="img/books/${data.uri}.png" alt="${data.uri}" width="210" height="300">
               </picture>
             </a>
-            <a class="product-card-mini__title-wrap" href="inner.html">
+            <a class="product-card-mini__title-wrap" href="description.html">
               <h3 class="product-card-mini__title">${data.name}</h3>
             </a>
             <p class="product-card-mini__descr">${data.desc}</p>
